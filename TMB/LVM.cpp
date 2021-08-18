ba2ce63c0578d0d22687ac66db7adc6e0bc3f4e0
MaximilianPi <maximilian.pichler@biologie.uni-regensburg.de>
2021-08-18 17:21:32 +0200

    Adding constrains to factor loading matrix

--- TMB/LVM.cpp
+++ TMB/LVM.cpp
@@ -1,29 +1,62 @@
+// Author: Max
 #include <TMB.hpp>
 
 template<class Type>
 Type objective_function<Type>::operator() ()
-{
+{ 
+  using namespace density;
   DATA_MATRIX(x);
   DATA_MATRIX(y);
   int s = y.cols();
   int n = x.rows();
-  
+
   PARAMETER_MATRIX(W);
-  PARAMETER_MATRIX(LF);
+  PARAMETER_VECTOR(LF);
   PARAMETER_MATRIX(LV);
+  int l = LV.cols();
+  int LV_size = LF.size();
   matrix<Type> fit(n, s);
+  matrix<Type> LF_constrained(l, s);
   parallel_accumulator<Type> nll(this);
   Type one=Type(1.0);
   vector<Type> prob;
-  int l = LV.cols();
   
   // prior
   for(int j = 0; j < l; j++) {
     nll -= sum( dnorm( vector<Type>(LV.col(j)), Type(0), Type(1), true) );
   }
   
-  fit = x*W + LV*LF;
+  for(int i = 0; i < LV_size; i++) {
+    nll -= ( dnorm( (Type)LF(i), Type(0), Type(1), true) );
+  }
   
+  for(int i = 0; i < s; i++) {
+    nll -= sum( dnorm( vector<Type>(W.col(i)), Type(0), Type(2), true) );
+  }
+  
+  /* construct factor loading matrix with the following constrains:
+      - upper triangular = 0 (doesn't work for high l? check Francis Hui's paper again)
+      - diagonal in [0, 1]
+      - rest in [-1, 1]
+  */
+  int counter = 0;
+  for(int j = 0;j < l; j++) {
+    for(int i = 0; i < s; i++) {
+      if(j>i) { LF_constrained(i, j) = 0; }
+      else if(j == i){ 
+        LF_constrained(j,i) = pnorm(LF(counter), Type(0),Type(1)); 
+        counter++;
+      } else {
+        LF_constrained(j,i) = Type(-1.0) + Type(2.0)* pnorm(LF(counter), Type(0),Type(1)); 
+        counter++;
+      }
+    }
+  }
+  
+  // predictions
+  fit = x*W + LV*LF_constrained;
+  
+  // likelihood
   for(int i=0;i<s;i++) {
     prob = vector<Type>( fit.col(i) );
     nll -= sum(  dbinom( vector<Type>(y.col(i)) ,Type(1.0) , vector<Type>( one/(one+exp(-prob)) ) , true)); 
--- code/playground.R
+++ code/playground.R
@@ -12,7 +12,7 @@ n = 200L # number of sites
 X = matrix(runif(e*n), n, e)
 W = matrix(rnorm(e*s), e, s)
 LF = matrix(rnorm(l*s), l, s)
-LV = mvtnorm::rmvnorm(n, c(0.0, 0.0))
+LV = mvtnorm::rmvnorm(n, rep(0, l))
 YR = X%*% W + LV%*%(LF)
 Y = apply(1/(1+exp(-YR)), 1:2, function(p) rbinom(1, 1, p))
 
@@ -23,8 +23,8 @@ dyn.load(dynlib("TMB/LVM"))
 
 ## Define parameters
 parameters = list(W=matrix(0.0, e, s) , 
-                  LF = matrix(rnorm(l*s), l, s), 
-                  LV = mvtnorm::rmvnorm(n, c(0.0, 0.0)))
+                  LF = as.vector(matrix(rnorm(l*s), l, s))[-(1:sum(1:(l-1)))], 
+                  LV = mvtnorm::rmvnorm(n, rep(0, l)))
 
 ## Make loss func
 model = MakeADFun(list(x=X, y = Y), parameters, DLL = "LVM", random = c("LV"))
@@ -33,11 +33,26 @@ model = MakeADFun(list(x=X, y = Y), parameters, DLL = "LVM", random = c("LV"))
 fit = nlminb(model$par, model$fn, model$gr)
 res = sdreport(model)
 
-W_TMB = matrix(fit$par, e, s)
-LF_TMB = matrix(fit$par[names(fit$par) %in% "LF"], l, s)
+W_TMB = matrix(fit$par[1:(e*s)], e, s)
+LF_TMB = matrix(0, s, l)
+counter = (e*s)+1
+for(j in 1:l) {
+  for(i in 1:s) {
+    if(j>i) { LF_TMB[i, j] = 0 }
+    else if (j == i) { 
+      LF_TMB[i, j] = pnorm(fit$par[counter])
+      counter= counter+1
+    } else {
+      LF_TMB[i, j] = -1+2*pnorm(fit$par[counter])
+      counter= counter+1
+    }
+  }
+}
+
+LF_TMB = t(LF_TMB)
 
 # Compare with gllvm
-gl = gllvm(Y, data.frame(X), family = binomial(), num.lv = 2L)
+gl = gllvm(Y, data.frame(X), family = binomial(), num.lv = l)
 W_GLLVM = t(coef(gl)$Xcoef)
 LF_GLLVM = t(coef(gl)$theta)
 
@@ -51,3 +66,4 @@ par(mfrow = c(1,3))
 plot_covariance(LF)
 plot_covariance(LF_TMB)
 plot_covariance(LF_GLLVM)
+
